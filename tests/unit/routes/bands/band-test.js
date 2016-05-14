4be4e1c2bf68a10c6d9e510b2b50e849e7d484ba
Jeff Weiss <jeff.weiss@puppet.com>
2016-05-13 23:34:55 -0700

    complete chapter 5

--- tests/unit/routes/bands/band-test.js
+++ tests/unit/routes/bands/band-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:application', 'Unit | Route | application', {
+moduleFor('route:bands/band', 'Unit | Route | bands/band', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
